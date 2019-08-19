f51fa3956f9402fe2fc437af5f307e11dbd44e88
Nauro da Silva Junior <nauro@outlook.com>
2019-08-19 12:02:58 -0300

    Remove var

--- index.js
+++ index.js
@@ -55,12 +55,11 @@ function getFlightData(flightArray){
 async function fetchFlights(aiportOne, airportTwo, destinyOne, destinyTwo, dia){
     const responseOrigin = await axios.get(`https://www.google.com/async/flights/search?vet=10ahUKEwju-PODoorkAhULDrkGHRuGAt4QjUMIaSgA..i&ei=SiNYXe6rFIuc5OUPm4yK8A0&yv=3&async=data:%5B%5B%5B%5B%5B%5Bnull%2C%5B%5B%22${aiportOne}%22%2C0%5D%2C%5B%22${airportTwo}%22%2C0%5D%5D%5D%2C%5Bnull%2C%5B%5B%22${destinyOne}%22%2C0%5D%2C%5B%22${destinyTwo}%22%2C0%5D%5D%5D%2C%5B%22${dia}%22%5D%5D%5D%2C2%2C%5B%5D%2Cnull%2Cnull%2Cnull%2Cnull%2C1%2Cnull%2C2%2Cnull%2Cnull%2Cnull%2Cnull%2Cnull%2Ctrue%5D%2C%5B%5B%5Bnull%2C%5B%5D%2C%5B%5D%2C%5B%5D%2C%5B%5D%2Cnull%2C%5B%5D%2C%5B%5D%5D%5D%5D%2Cnull%2C%22BRL%22%2Cnull%2C%5B%5D%5D%2C%5B0%5D%5D,s:s,tfg-bgr:%5B%22!BwSlBCVCsci0balv2-5EMiA8Wsh02u4CAAAAtlIAAAAXmQGNgo9p2O0075OInX1JfYbSyBM53SBjBPZ6DaD1_Cf9-zB0fOubOVcVIL8mLe9APu-0M0nGvVn-eLHqOSDAnYL40Kwg1ePSQRWSPAZTJ9ftaIgdPKKzQpxrcs8EAJyMbZrGW8fsgmRGimY8zQrvyJQ-BhI1PNlJTB2ZsFZQnpdcU1FdPR1Z6UppUimmRlBDHduMqly6tN_tFJTKbPW3FkK5s_86VxVOzi7A8CAHs1Ao8cFl4RXnM11y_Z7nF1LUBjSp6G71PJGDBVtq1HVknto3x13TaDtw9C4FvGJAhVEhHtEiJ3-IZPy3LM0BTkqsbYG4u7thBr54ixR-Ey_6U8VeBPdnDNWtOUZRIw-q-NlFBfDGSppqsRUJ8s9HHy3HMV8Gb4YrAs1_gLsCGmA0BuwTrqd0eL11qdRZBP8c5rJdWrwQGy-33dyd-dloVDjh0uQHjt6wvDImDpg22XRMc6BBT_anghxJC2BhuRvDI5O6c2ZaLEtN64kWIYrqiMDz8ONakQnyEtfc45-vtPPANg%22%2Cnull%2Cnull%2C24%2Cnull%2Cnull%2Cnull%2C0%5D,_fmt:jspb`);
     const results = JSON.parse(responseOrigin.data.substr(5));
-    
-    var allFlights = [];
      
-    allFlights = getPreferredFlights(results);
-    return allFlights.concat(getOtherFlights(results));
+    const preferredFlights = getPreferredFlights(results);
+    const otherFlights = getOtherFlights(results);
 
+    return preferredFlights.concat(otherFlights);
 }
 
 function filterFlights(flights, time){
@@ -88,3 +87,4 @@ async function run(){
 }
 
 run();
+
